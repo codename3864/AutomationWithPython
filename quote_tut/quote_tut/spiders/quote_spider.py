b1a501f11dddc6ec3dd793a1be60d68f5f8c74d2
Vishal Kumar <vr16796@gmail.com>
2020-01-11 02:58:40 +0530

    data crawl finished

--- quote_tut/quote_tut/spiders/quote_spider.py
+++ quote_tut/quote_tut/spiders/quote_spider.py
@@ -10,24 +10,19 @@ class QuoteSpider(scrapy.Spider):
     start_urls = []
     for i in index:
         start_urls.append(start_url+str(i))
-    
 
-    # def start_requests(self):
-    #     urls = [
-    #         'http://quotes.toscrape.com/',
-    #         # 'http://quotes.toscrape.com/page/2/',
-    #         ]
-    #     for url in urls:
-    #         yield scrapy.Request(url=url, callback=self.parse)
+
 
     def parse(self, response):
-        title = response.css('title::text').extract()
+        all_quotes = response.css('div.quote')
 
-
-        yield {'title':title}
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
+        for quotes in all_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tags = quotes.css('.tag::text').extract()
+            yield {
+                'title' : title,
+                'author':author,
+                'tags' : tags
+            }
 
